2227f0cacc1069a559c925ebb5c7376aee4a693d
khalil-Alkadahi <khalilalkadahi@gmail.com>
2025-11-01 01:46:12 +0300

    add show modal bottom sheet

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 
-import 'custom_note_item.dart';
 import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
