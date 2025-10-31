eec8dcf2610ccf8b0148b36fd9d3657dc9e28f2c
khalil-Alkadahi <khalilalkadahi@gmail.com>
2025-11-01 02:26:10 +0300

    add fields to notes bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -9,9 +9,11 @@ class AddNoteBottomSheet extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
-        children: [
+        children:const [
           SizedBox( height: 32,),
-          CustomTextField(),
+          CustomTextField(hint: 'Title',),
+          SizedBox(height: 16,),
+          CustomTextField(hint: 'Content',maxLine: 5,),
         ],
       ),
     );
