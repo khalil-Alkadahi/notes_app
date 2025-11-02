4185b781b06724d536f02eff972df459186b6a26
khalil-Alkadahi <khalilalkadahi@gmail.com>
2025-11-03 01:42:07 +0300

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,40 +1,48 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24,bottom: 24,left: 16,),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: const Color(0xffFFCC80),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title:const Text('Flutter Tips', style: TextStyle(color: Colors.black,fontSize: 26)),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16,bottom: 16),
-              child: Text(
-                'Bulid your career with khalil alkadahi',
-                style: TextStyle(color: Colors.black.withValues(alpha: 0.5),fontSize: 18),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        },));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24,bottom: 24,left: 16,),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: const Color(0xffFFCC80),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title:const Text('Flutter Tips', style: TextStyle(color: Colors.black,fontSize: 26)),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16,bottom: 16),
+                child: Text(
+                  'Bulid your career with khalil alkadahi',
+                  style: TextStyle(color: Colors.black.withValues(alpha: 0.5),fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(FontAwesomeIcons.trash, color: Colors.black,size: 24,),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(FontAwesomeIcons.trash, color: Colors.black,size: 24,),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text('May 21 , 2022', style: TextStyle(color: Colors.black.withValues(alpha: 0.5),fontSize: 16)),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text('May 21 , 2022', style: TextStyle(color: Colors.black.withValues(alpha: 0.5),fontSize: 16)),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
