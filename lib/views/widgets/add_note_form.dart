5d0d57c82ee238546a52ea4260bb44c5e6a273a6
khalil-Alkadahi <khalilalkadhi@gmail.com>
2025-11-08 02:42:08 +0300

    adding add note cubit to add note bottom sheet

---
+++ lib/views/widgets/add_note_form.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+
+import 'custom_button.dart';
+import 'custom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey =GlobalKey();
+  AutovalidateMode autovalidateMode =AutovalidateMode.disabled;
+
+  String? title ,subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox( height: 32,),
+          CustomTextField(hint: 'Title',onSaved: (value) {
+            title=value;
+          },),
+          const SizedBox(height: 18,),
+          CustomTextField(hint: 'Content',maxLine: 5,onSaved: (value) {
+            subTitle=value;
+          },),
+          const  SizedBox(height: 64,),
+          CustomButton(onTap: () {
+            if(formKey.currentState!.validate()){
+              formKey.currentState!.save();
+            }else{
+              autovalidateMode= AutovalidateMode.always;
+              setState(() {
+
+              });
+            }
+          },),
+          SizedBox(height: 32,),
+        ],
+      ),
+    );
+  }
+}
