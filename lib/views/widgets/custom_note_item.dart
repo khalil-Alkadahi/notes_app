fbc197048cca59ce36829cef3a28e9d15525c6ee
khalil-Alkadahi <khalilalkadhi@gmail.com>
2025-11-09 04:03:43 +0300

    display notes part two

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,45 +1,66 @@
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
+  const NoteItem({super.key, required this.note});
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return const EditNoteView();
-        },));
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
       },
       child: Container(
-        padding: const EdgeInsets.only(top: 24,bottom: 24,left: 16,),
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
-          color: const Color(0xffFFCC80),
+          color:  Color(note.color),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title:const Text('Flutter Tips', style: TextStyle(color: Colors.black,fontSize: 26)),
+              title:  Text(
+                note.title,
+                style: const TextStyle(color: Colors.black, fontSize: 26),
+              ),
               subtitle: Padding(
-                padding: const EdgeInsets.only(top: 16,bottom: 16),
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
                 child: Text(
-                  'Bulid your career with khalil alkadahi',
-                  style: TextStyle(color: Colors.black.withValues(alpha: 0.5),fontSize: 18),
+                  note.subTitle,
+                  style: TextStyle(
+                    color: Colors.black.withValues(alpha: 0.5),
+                    fontSize: 18,
+                  ),
                 ),
               ),
               trailing: IconButton(
                 onPressed: () {},
-                icon: Icon(FontAwesomeIcons.trash, color: Colors.black,size: 24,),
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(right: 24),
-              child: Text('May 21 , 2022', style: TextStyle(color: Colors.black.withValues(alpha: 0.5),fontSize: 16)),
+              child: Text(
+                note.date,
+                style: TextStyle(
+                  color: Colors.black.withValues(alpha: 0.5),
+                  fontSize: 16,
+                ),
+              ),
             ),
           ],
         ),
