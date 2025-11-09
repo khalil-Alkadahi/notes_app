f31ff41c3827f6b12ab2bc059952f692d3cfd18b
khalil-Alkadahi <khalilalkadhi@gmail.com>
2025-11-09 03:48:56 +0300

    display notes part one

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -11,9 +11,3 @@ final class NotesSuccess extends NotesState {
 
   NotesSuccess(this.notes);
 }
-final class NotesFailure extends NotesState {
-
-  final String errMessage;
-
-  NotesFailure(this.errMessage);
-}
